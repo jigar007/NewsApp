0b281b457f64e83835a57ea36887855b7cff9495
Jigar Thakkar <jigar@kintell.com>
2021-09-06 00:50:48 +1000

    Added webview to detail view

--- NewsApp/NewDetail/NewsDetailViewController.swift
+++ NewsApp/NewDetail/NewsDetailViewController.swift
@@ -6,21 +6,36 @@
 //
 
 import UIKit
+import WebKit
 
 class NewsDetailViewController: UIViewController {
 
     private var news: News
+    private let wkWebView = WKWebView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        let backButton = UIBarButtonItem()
+        backButton.title = ""
+        self.navigationController?.navigationBar.topItem?.backBarButtonItem = backButton
+
+        if let newsURL = news.url {
+            wkWebView.frame = view.bounds
+            wkWebView.load(URLRequest(url: newsURL))
+            view.addSubview(wkWebView)
+        } else {
+            let newsURLNotAvailable = UILabel(frame: CGRect(x: 0, y: 0, width: 400, height: 20))
+            newsURLNotAvailable.textAlignment = .center
+            newsURLNotAvailable.numberOfLines = 0
+            newsURLNotAvailable.text = "Source URL for new article is not available"
+            newsURLNotAvailable.center = view.center
+            view.addSubview(newsURLNotAvailable)
+        }
     }
 
     init(news: News) {
         self.news = news
-
-        debugPrint(news)
-
         super.init(nibName: nil, bundle: nil)
     }
     
